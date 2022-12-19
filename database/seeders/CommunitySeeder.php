4ae4286ec896d9ac1a826a2fc0c7e680c895ce14
Josias Pereira Bueno <66849846+buenojosias@users.noreply.github.com>
2022-12-19 17:06:41 -0300

    Community: model, migration e seeder

---
+++ database/seeders/CommunitySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CommunitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Community::create([
+            'name' => 'Paróquia São Marcos',
+        ]);
+        Community::create([
+            'name' => 'Capela Beato Giácomo Cusmano',
+        ]);
+        Community::create([
+            'name' => 'Capela Nossa Senhora da Misericórdia',
+        ]);
+        Community::create([
+            'name' => 'Capela Nossa Senhora da Perseverança',
+        ]);
+    }
+}
