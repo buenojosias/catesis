dfd787c4d85afec3d821db8482545847ad25a198
Josias Pereira Bueno <66849846+buenojosias@users.noreply.github.com>
2023-01-10 23:54:31 -0300

    Grade: corrigido catequizandos por comunidade

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Community;
+use App\Models\Student;
 use App\Models\Grade;
 use App\Models\Group;
 use Illuminate\Http\Request;
 
 class GradeController extends Controller
 {
+    public function __construct() {
+        $grade;
+    }
+
     public function index()
     {
         return view('grades.index');
@@ -17,26 +22,18 @@ class GradeController extends Controller
     public function show(Grade $grade)
     {
         if(auth()->user()->hasRole('admin')) {
-            /*$groups = Group::query()
-                ->where('finished', false)
-                ->where('grade_id', $grade->id)
-                ->with('community')
-                ->withCount('students')
-                ->get()
-                ->groupBy('community_id')
-                ->sum('students');*/
-
-            /*$communities = Community::query()
-                ->with('groups', function($query) {
-                    return $query->where('grade_id', $grade->id)->withCount('students');
-                })
-                ->get();*/
-
-            //return($communities);
+            $this->grade = $grade;
+            $communities = Community::query()->get();
+            $communities->loadCount(['active_students' => function($query) {
+                $query->where('grade_id', $this->grade->id);
+            }]);
         }
+        $themes = $grade->themes()->orderBy('sequence', 'asc')->get();
 
         return view('grades.show', [
             'grade' => $grade,
+            'themes' => $themes,
+            'communities' => $communities ?? null,
         ]);
     }
 }
--- resources/views/grades/show.blade.php
+++ resources/views/grades/show.blade.php
@@ -11,13 +11,47 @@
             <p>{{ $grade->description ?? 'Nenhuma descrição disponível.' }}</p>
         </div>
     </div>
+    <div class="mt-4 grid grid-cols-2 gap-4">
+        @hasrole('admin')
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h3>Catequizandos por comunidade</h3>
+                </div>
+                <div class="card-body table-responsive">
+                    <table class="table">
+                        <tbody>
+                            @foreach ($communities as $community)
+                                <tr>
+                                    <td>{{ $community->name }}</td>
+                                    <td class="text-right">{{ $community->active_students_count }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @endhasrole
+        <div class="card mb-4">
+            <div class="card-header">
+                <h3>Temas</h3>
+            </div>
+            <div class="card-body table-responsive">
+                <table class="table">
+                    <tbody>
+                        @foreach ($themes as $theme)
+                            <tr>
+                                <td>{{ $theme->sequence }}. {{ $theme->title }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
     <h4 class="mt-4 font-bold">Recursos</h4>
     <ul>
         <li>- Nome / descrição</li>
         <li>- Quantidade de catequizandos</li>
-        <li>- Quantidade de catequizandos por comunidade (se admin)</li>
-        <li>- Temas</li>
-        <li>- Turmas *?</li>
     </ul>
 </x-app-layout>
