9b09359443493fefa9565c82d4a3dfdbdc2f8804
Josias Pereira Bueno <66849846+buenojosias@users.noreply.github.com>
2023-02-05 22:26:20 -0300

    Atualizações 05fev2022

--- resources/views/layouts/printable.blade.php
+++ resources/views/layouts/printable.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/printable.css'])
+    <link rel="stylesheet" href="{{ asset('build/assets/printable-01eb661f.css') }}">
+    {{-- @vite(['resources/css/printable.css']) --}}
     <title>{{ $title }}</title>
 </head>
 <body>
